7ee1b708a447ec842cc0a1d36d794a8e5357cafd
John Gitahi <gitahi109@gmail.com>
2022-12-15 14:30:03 +0300

    Style auction page

--- src/components/auction/Swap.jsx
+++ src/components/auction/Swap.jsx
@@ -17,17 +17,20 @@ const Swap = () => {
                 <h3>Top bids from popular creators</h3>
                 <div className={styles["row"]}>
                     <div className={styles["column"]}>
-                        <div className={styles['card']}>
+                        <div className={styles.card}>
                             <div className={styles.top}>
                                 <button>♥ Like</button>
                             </div>
                             <img src={topauctions[0]['img']} />
                             <h3>Out of the Box</h3>
                         </div>
-                        <div className={styles['extrainfo']}>
-                            <h4>Creator: Dan Murray</h4>
-                            <h4>Date: 12/08/22</h4>
-                            <h4>Highest Bid: 0.57ETH</h4>
+                        <div className={styles.extrainfo}>
+                            <h4><span className="gray">Creator:</span> Dan Murray</h4>
+                            <h4><span className="gray">Date:</span> 12/08/22</h4>
+                            <h4><span className="gray">Highest Bid:</span> 0.57ETH</h4>
+                        </div>
+                        <div className={styles.placebid}>
+                            {/* Add place a bid button*/}
                         </div>
                     </div>
                     <div className={styles["column"]}>
@@ -38,10 +41,13 @@ const Swap = () => {
                             <img src={topauctions[1]['img']} />
                             <h3>Falling Apart</h3>
                         </div>
-                        <div className={styles['extrainfo']}>
-                            <h4>Creator: Jacob Banks</h4>
-                            <h4>Date: 12/08/22</h4>
-                            <h4>Highest Bid: 0.34ETH</h4>
+                        <div className={styles.extrainfo}>
+                            <h4><span className="gray">Creator:</span> Jacob Banks</h4>
+                            <h4><span className="gray">Date:</span> 12/08/22</h4>
+                            <h4><span className="gray">Highest Bid:</span> 0.34ETH</h4>
+                        </div>
+                        <div className={styles.placebid}>
+                            {/* Add place a bid button*/}
                         </div>
                     </div>
                 </div>
