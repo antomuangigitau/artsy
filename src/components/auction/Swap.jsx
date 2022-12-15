f01a1efcf23314ecef565b800b5f5c1555b76230
John Gitahi <gitahi109@gmail.com>
2022-12-15 13:25:46 +0300

    Fix view of other auctions

--- src/components/auction/Swap.jsx
+++ src/components/auction/Swap.jsx
@@ -13,35 +13,36 @@ const Swap = () => {
                     return <img src={img} key={id}/>;
                 })}
             </div>
-            <div className={styles["top-bids"]}>
+            <div className={styles["top__bids"]}>
                 <h3>Top bids from popular </h3>
                 <div className={styles["row"]}>
                     <div className={styles["column"]}>
-                        <div className={styles['left']}>
-                            <div className="top">
-                                <button>♥</button>
+                        <div className={styles['card']}>
+                            <div className={styles.top}>
+                                <button>♥ Like</button>
                             </div>
                             <img src={topauctions[0]['img']} />
                             <h3>Out of the Box</h3>
                         </div>
-                        <div className="extrainfo">
+                        <div className={styles['extrainfo']}>
                             <h4>Creator: Dan Murray</h4>
                             <h4>Date: 12/08/22</h4>
                             <h4>Highest Bid: 0.57ETH</h4>
                         </div>
                     </div>
                     <div className={styles["column"]}>
-                        <div className={styles['right']}>
-                            <div className="top">
-                                <button>♥</button>
+                        <div className={styles['card']}>
+                            <div className={styles.top}>
+                                <button>♥ Like</button>
                             </div>
                             <img src={topauctions[1]['img']} />
                             <h3>Falling Apart</h3>
                         </div>
-                        <div className="extrainfo"></div>
-                        <h4>Creator: Dan Murray</h4>
-                        <h4>Date: 12/08/22</h4>
-                        <h4>Highest Bid: 0.34ETH</h4>
+                        <div className={styles['extrainfo']}>
+                            <h4>Creator: Jacob Banks</h4>
+                            <h4>Date: 12/08/22</h4>
+                            <h4>Highest Bid: 0.34ETH</h4>
+                        </div>
                     </div>
                 </div>
             </div>
